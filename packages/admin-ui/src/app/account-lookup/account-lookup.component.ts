23a769d2322a3b6aad2adefdac7465110c35de7b
Rui Rocha <rui.rocha.work@outlook.com>
2023-02-27 20:23:08 +0000

    fixed wrong currency value form being used in the operations of the account lookup component

--- packages/admin-ui/src/app/account-lookup/account-lookup.component.ts
+++ packages/admin-ui/src/app/account-lookup/account-lookup.component.ts
@@ -158,7 +158,7 @@ export class AccountLookupComponent implements OnInit {
   }
 
   async associateParticipant() {
-    this._interopSvc.associateParticipant(this.formAssociateParticipant.controls["participantId"].value, this.formAssociateParticipant.controls["partyType"].value, this.formAssociateParticipant.controls["partyId"].value, this.formAssociateParticipant.controls["partySubType"].value, this.formGetParticipant.controls["currencyCode"].value).subscribe(result => {
+    this._interopSvc.associateParticipant(this.formAssociateParticipant.controls["participantId"].value, this.formAssociateParticipant.controls["partyType"].value, this.formAssociateParticipant.controls["partyId"].value, this.formAssociateParticipant.controls["partySubType"].value, this.formAssociateParticipant.controls["currencyCode"].value).subscribe(result => {
       if(result) {
         this._messageService.addSuccess("associateParticipant event sucessfully sent");
       }else{
@@ -168,7 +168,7 @@ export class AccountLookupComponent implements OnInit {
   }
 
   async disassociateParticipant() {
-    this._interopSvc.disassociateParticipant(this.formDisassociateParticipant.controls["participantId"].value, this.formDisassociateParticipant.controls["partyType"].value, this.formDisassociateParticipant.controls["partyId"].value, this.formDisassociateParticipant.controls["partySubType"].value, this.formGetParticipant.controls["currencyCode"].value).subscribe(result => {
+    this._interopSvc.disassociateParticipant(this.formDisassociateParticipant.controls["participantId"].value, this.formDisassociateParticipant.controls["partyType"].value, this.formDisassociateParticipant.controls["partyId"].value, this.formDisassociateParticipant.controls["partySubType"].value, this.formDisassociateParticipant.controls["currencyCode"].value).subscribe(result => {
       if(result) {
         this._messageService.addSuccess("disassociateParticipant event sucessfully sent");
       }else{
@@ -188,7 +188,7 @@ export class AccountLookupComponent implements OnInit {
   }
 
   async getParty() {
-    this._interopSvc.getParty(this.formGetParty.controls["participantId"].value, this.formGetParty.controls["partyType"].value, this.formGetParty.controls["partyId"].value, this.formGetParty.controls["partySubType"].value, this.formGetParticipant.controls["currencyCode"].value).subscribe(result => {
+    this._interopSvc.getParty(this.formGetParty.controls["participantId"].value, this.formGetParty.controls["partyType"].value, this.formGetParty.controls["partyId"].value, this.formGetParty.controls["partySubType"].value, this.formGetParty.controls["currencyCode"].value).subscribe(result => {
       if(result) {
         this._messageService.addSuccess("getParty event sucessfully sent");
       }else{
