d52ea180af37936de2a4b6e4d6d3d269c382b1af
Tom Daly <tdaly61@gmail.com>
2023-09-27 04:22:42 +0000

    small fix to arm64 work around

--- packages/installer/mini-loop/interop-interim-fix.sh
+++ packages/installer/mini-loop/interop-interim-fix.sh
@@ -7,33 +7,38 @@
 function set_user {
   # set the k8s_user 
   k8s_user=`who am i | cut -d " " -f1`
+  echo "k8s_user = $k8s_user"
 }
 
-# ensure we are running as root 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 1
-fi
 
 #### main #####
-
+BASE_DIR="$( cd $(dirname "$0")/../../.. ; pwd )"
+echo "DBG> BASE_DIR = $BASE_DIR"
+MANIFESTS_DIR=$BASE_DIR/packages/installer/manifests
+local_image="local-fspiop-api" 
 # ensure we are running as root 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit 1
 fi
 set_user
-echo "k8s_user = $k8s_user"
+
+# clone the interop BC repo 
 rm -rf /tmp/interop-apis-bc
 su - $k8s_user -c "git clone https://github.com/mojaloop/interop-apis-bc.git /tmp/interop-apis-bc"
 local_version=` cat /tmp/interop-apis-bc/packages/fspiop-api-svc/package.json | grep version | cut -d ":" -f2 | tr -d "\"" | tr -d "," | tr -d " "`
 echo "local version : $local_version"
-# cd /tmp/interop-apis-bc
-su - $k8s_user -c  "cd /tmp/interop-apis-bc; docker build -f packages/fspiop-api-svc/Dockerfile -t fspiop-api:$local_version . " 
-tarfile="/tmp/fspiop-api:$local_version.tar" 
-echo "tarfile is $tarfile"
-rm -f $tarfile
-su - $k8s_user -c "docker save --output $tarfile fspiop-api:$local_version"
 
-ctr images import "$tarfile"
+# build a version of the image 
+su - $k8s_user -c  "cd /tmp/interop-apis-bc; docker build -f packages/fspiop-api-svc/Dockerfile -t $local_image:$local_version . " 
+
+# save this docker image out and export to containerd images 
+tarfile="/tmp/$local_image:$local_version.tar" 
+echo "tarfile is $tarfile"
+rm -f $tarfile  # remove any old ones lying around 
+su - $k8s_user -c "docker save --output $tarfile $local_image:$local_version"
+k3s ctr images import "$tarfile"
 rm -f $tarfile
+
+# now modify the interop deloyment yaml to use this local image and set set imagePullPolicy to Never 
+su - $k8s_user -c "perl -p -i.bak -e 's/image:.*$/image: $local_image:$local_version/g' $MANIFESTS_DIR/apps/fspiop-api-svc-deployment.yaml"
