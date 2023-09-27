7051430fe5635356e4fac5d45af84819e80f8bc0
Tom Daly <tdaly61@gmail.com>
2023-09-27 08:28:55 +0000

    image restore fixed properly

--- packages/installer/mini-loop/mini-loop-vnext-install.sh
+++ packages/installer/mini-loop/mini-loop-vnext-install.sh
@@ -9,43 +9,6 @@
 
 source ../scripts/common.sh 
 
-
-# function update_k8s_images_from_docker_files {
-#   local yaml_files=("path/to/file1.yaml" "path/to/file2.yaml")  # Replace with your YAML file paths
-#   compose_dir=$BASE_DIR/packages/deployment  
-#   CURRENT_IMAGES_FROM_DOCKER_FILES=($(grep image $compose_dir/**/docker*yml | grep -v infra | grep mojaloop | cut -d ":" -f3,4))
-#   MANIFESTS_DIR1="/tmp/manifests"
-#   k8s_yaml_files=($(ls $MANIFESTS_DIR1/**/*yaml))
-#   # for element in "${k8s_yaml_files[@]}"; do
-#   #   echo "$element"
-#   # done
-#   # for element in "${CURRENT_IMAGES_FROM_DOCKER_FILES[@]}"; do
-#   #   echo "$element"
-#   # done
-#   #local image_array=("image1:tag1" "image2:tag2")  # Replace with your array of images
-
-#   for ((i=0; i<${#CURRENT_IMAGES_FROM_DOCKER_FILES[@]}; i++)); do
-#     local image="${CURRENT_IMAGES_FROM_DOCKER_FILES[$i]}"
-#     local image_name="${image%%:*}"  # Extract the image name
-#     local image_tag="${image#*:}"    # Extract the image tag
-    
-#     echo "i : $i"
-#     echo "image: $image"
-#     echo "image_name: $image_name"
-#     echo "image_tag: $image_tag"
-
-#     for yaml_file in "${k8s_yaml_files[@]}"; do
-#       if grep -q "image: $image_name" "$yaml_file"; then
-#         sed -i "s|image: $image_name:.*|image: $image_name:$image_tag|g" "$yaml_file"
-#         #echo "Updated image in $image_name:$image_tag in $yaml_file "
-#       fi 
-
-#     done
-#   done
-# }
-
-
-
 ################################################################################
 # Function: showUsage
 ################################################################################
