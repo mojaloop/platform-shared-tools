c70b50c39f7c41ca59628da54af6169d2d2c71d8
Tom Daly <tdaly61@gmail.com>
2023-11-27 01:03:49 +0000

    [skip ci] dns and tls wip

--- packages/installer/scripts/vnext-configure.py
+++ packages/installer/scripts/vnext-configure.py
@@ -87,30 +87,64 @@ def allocate_infra_pods(p,yaml):
             yaml.dump(data,f)
 
 
-def modify_values_for_dns_domain_name(p,domain_name,verbose=False):
-    print(f" --domain_name ")
-    mydomain="vnext.mojaloop.fred"
+def modify_yaml_for_dns_domain_name(p,domain_name,verbose=False):
+    print(f" --domain_name {domain_name}")
     #modify ingress hostname in values file to use DNS name     
     print(f"      <mojaloop-configure.py> : Modify values to use dns domain name {domain_name}" )
     # for vf in p.glob('**/*values.yaml') :
+    # first the ingress.yaml files 
     p1 = Path("/tmp/x")
-    for vf in p1.glob('**/*ingress*.yaml') :
+    for vf in p1.glob('**/*values.yaml') :
         print(f"file is {vf}")
         with FileInput(files=[str(vf)], inplace=True) as f:
             for line in f:
                 line = line.rstrip()
-                line = re.sub(r"#external-dns.alpha.kubernetes", "external-dns.alpha.kubernetes", line)
+                line = re.sub(r"#external-dns.alpha.kubernetes.io/hostname:(\s+)(\w+).*$", f"external-dns.alpha.kubernetes.io/hostname:\\1\\2.{domain_name}", line)
                 line = re.sub(r"#cert-manager.io/issuer:", "cert-manager.io/issuer:", line)
-                line = re.sub(r"# tls:", "tls:", line)
+                line = re.sub(r"#tls:", "tls:", line)
                 line = re.sub(r"#(\s+)- hosts:", "  - hosts:", line)
-                line = re.sub(r"#(\s+)- (\S+).vnext.mojaloop.live", f"      - \\2.{mydomain}", line)
+                line = re.sub(r"#(\s+)- (\S+).local", f"      - \\2.{domain_name}", line)
                 line = re.sub(r"#(\s+)secretName: quickstart-example-tls", "    secretName: quickstart-example-tls", line)
+                line = re.sub(r"(\s+)- host: (\w+).local", f"\\1- host: \\2.{domain_name}", line)
+                line = re.sub(r"(\s+)hostname: (\S+).local", f"\\1hostname: \\2.{domain_name}", line)
 
-        #         line = re.sub(r"(\s+)hostname: (\S+).local", f"\\1hostname: \\2.{domain_name}", line)
+                # tls and dns changes specific to helm values.yaml 
+                line = re.sub(r"#annotations.*$", "annotations", line)
+                line = re.sub(r"#(\s+)- secretName: quickstart-example-tls.*$", "\\1- secretName: quickstart-example-tls", line)
+                line = re.sub(r"#(\s+)hosts:.*$", "\\1hosts:", line)
+                line = re.sub(r"#(\s+)-(\w+).local.*$", f"\\1- \\2.{domain_name}", line)
 
-        #         line = re.sub(r"(\s+)host: (\S+).local", f"\\1host: \\2.{domain_name}", line)
+
+
+
         #         line = re.sub(r"testing-toolkit.local", f"testing-toolkit.{domain_name}", line)
                 print(line)
+
+# def modify_yaml_for_certs(p,verbose=False):
+#     print(f" --domain_name {domain_name}")
+#     #modify ingress hostname in values file to use DNS name     
+#     print(f"      <mojaloop-configure.py> : Modify values to use dns domain name {domain_name}" )
+#     # for vf in p.glob('**/*values.yaml') :
+#     # first the ingress.yaml files 
+#     p1 = Path("/tmp/x")
+#     for vf in p1.glob('**/*.yaml') :
+#         print(f"file is {vf}")
+#         with FileInput(files=[str(vf)], inplace=True) as f:
+#             for line in f:
+#                 line = line.rstrip()
+#                 line = re.sub(r"#external-dns.alpha.kubernetes.io/hostname:(\s+)(\w+).*$", f"external-dns.alpha.kubernetes.io/hostname:\\1\\2.{domain_name}", line)
+#                 line = re.sub(r"#cert-manager.io/issuer:", "cert-manager.io/issuer:", line)
+#                 line = re.sub(r"# tls:", "tls:", line)
+#                 line = re.sub(r"#(\s+)- hosts:", "  - hosts:", line)
+#                 line = re.sub(r"#(\s+)- (\S+).vnext.mojaloop.live", f"      - \\2.{domain_name}", line)
+#                 line = re.sub(r"#(\s+)secretName: quickstart-example-tls", "    secretName: quickstart-example-tls", line)
+
+#         #         line = re.sub(r"(\s+)hostname: (\S+).local", f"\\1hostname: \\2.{domain_name}", line)
+
+#         #         line = re.sub(r"(\s+)host: (\S+).local", f"\\1host: \\2.{domain_name}", line)
+#         #         line = re.sub(r"testing-toolkit.local", f"testing-toolkit.{domain_name}", line)
+#                 print(line)
+
 
 
 def parse_args(args=sys.argv[1:]):
@@ -147,7 +181,7 @@ def main(argv) :
         print(f" allocate pods to infra node_class nodes -- assumes node pool exists for now ")
         allocate_infra_pods(p,yaml)
     if args.domain_name :
-         modify_values_for_dns_domain_name(p,args.domain_name,args.verbose)
+         modify_yaml_for_dns_domain_name(p,args.domain_name,args.verbose)
     if args.verbose :
         print(f"     <vnext_configure.py>  : end ")
 
