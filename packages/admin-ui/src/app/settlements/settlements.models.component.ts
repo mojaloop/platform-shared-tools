e0ef26f95f790759c7755239c89d10a3d6ce2a22
Zw3 <pyroflame04@gmail.com>
2024-05-23 16:14:35 +0630

    fix: settlement models page error msg not showing fixed

--- packages/admin-ui/src/app/settlements/settlements.models.component.ts
+++ packages/admin-ui/src/app/settlements/settlements.models.component.ts
@@ -32,9 +32,12 @@ export class SettlementsModelsComponent implements OnInit {
 			this._settlementsService.getAllModels().subscribe(matrix => {
 				this.models.next(matrix);
 				resolve();
+			}, error => {
+				if (error && error instanceof UnauthorizedError) {
+					this._messageService.addError(error.message);
+				}
 			});
 		});
-
 	}
 
 
