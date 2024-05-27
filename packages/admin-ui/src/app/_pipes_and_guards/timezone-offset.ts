70e10f4b981ae20aa7f69042c304c90790bbfaa3
stmSi <stm1998sithumyo@gmail.com>
2024-05-27 12:06:13 +0630

    fix: npm run lint

--- packages/admin-ui/src/app/_pipes_and_guards/timezone-offset.ts
+++ packages/admin-ui/src/app/_pipes_and_guards/timezone-offset.ts
@@ -6,7 +6,6 @@ import * as moment from 'moment-timezone';
 })
 export class TimezoneOffsetPipe implements PipeTransform {
 
-  constructor() {}
   transform(timezone: string = ''): string {
 	const offset = moment().format('Z'); // e.g., +05:30 or -04:00
     return `UTC${offset}`;
