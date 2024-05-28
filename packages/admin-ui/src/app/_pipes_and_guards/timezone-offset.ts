a5cda6a4966fca00a286eb5384d9b4a472a675e7
stmSi <stm1998sithumyo@gmail.com>
2024-05-28 15:57:00 +0630

    fix: comment why pipe is used for getting timezoneoffset from template

--- packages/admin-ui/src/app/_pipes_and_guards/timezone-offset.ts
+++ packages/admin-ui/src/app/_pipes_and_guards/timezone-offset.ts
@@ -5,9 +5,15 @@ import * as moment from 'moment-timezone';
   name: 'timezoneOffset'
 })
 export class TimezoneOffsetPipe implements PipeTransform {
-
+  /**
+   * This pipe is used to get the timezone offset of the current time.
+   * Using a pipe allows reuse in templates without the need to redefine in each *.component.ts file.
+   *
+   * @param timezone (optional) - Currently unused.
+   * @returns A string representing the current UTC offset, e.g., 'UTC+05:30' or 'UTC-04:00'.
+   */
   transform(timezone: string = ''): string {
-	const offset = moment().format('Z'); // e.g., +05:30 or -04:00
+	const offset = moment().format('Z');
     return `UTC${offset}`;
   }
 }
